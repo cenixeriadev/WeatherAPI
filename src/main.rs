22200310b8f7efc43667df4e104d6a09cccf3c86
Anthony ramos roncal <codeartprogrammer@gmail.com>
2025-03-08 11:04:15 -0500

    feat : Validate fields and fix bug in UI.

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ use weather_app_ui::WeatherApp;
 
 fn main() -> Result<(), eframe::Error> {
     let options = NativeOptions::default();
-    eframe::run_native("Aplicación de Clima", options, Box::new(|_cc| Box::new(WeatherApp::default())))
+    eframe::run_native("Weather App", options, Box::new(|_cc| Box::new(WeatherApp::default())))
 }
